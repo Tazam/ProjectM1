5714dc6b7444af47f8a8d73951c7454afe9815b4
Poisson Blob <Poisson Blob@PoissonBlob-PC>
2018-04-06 23:52:03 +0200

    ajout d'un test directement via l'annotator

--- src/test/Test_ssplit.java
+++ src/test/Test_ssplit.java
@@ -1,9 +1,11 @@
 package test;
 
 import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.simple.Sentence;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 
@@ -15,12 +17,15 @@ public class Test_ssplit
 {
 	public static void main(String[] args) throws IOException 
 	{
-		// Lecture du contenu d'un texte du corpus. Il faut télécharger et
-		// ajouter au path org.apache.commons.io.IOUtils;
-		String path = "corpus" + File.separator + "bnw_page1.txt";
-		FileInputStream is = new FileInputStream(path);     
-		String content = IOUtils.toString(is, "UTF-8");
-
+		TestPipeLine();
+		TestWordsToSentencesAnnotator();
+	}
+	
+	public static void TestPipeLine() throws IOException
+	{
+		System.out.println("Ceci est un test de ssplit via le pipeline");
+		String content = lireExemple();
+		
 		// Construction du CoreDocument
 		Properties props = new Properties();
 		// pour tester ssplit, on a besoin de tokenize
@@ -41,6 +46,38 @@ public class Test_ssplit
 			String sentenceText = sentence.text();
 			System.out.println("Phrase " + i + " : " + sentenceText);
 		}
-  }
+	}
+	
+	public static void TestWordsToSentencesAnnotator() throws IOException
+	{
+		System.out.println("Ceci est un test de ssplit via le WordsToSentencesAnnotator");
+		String content = lireExemple();
+		
+		Annotation annotation = new Annotation(content);
+		
+		TokenizerAnnotator required = new TokenizerAnnotator();
+		required.annotate(annotation);
+		
+		WordsToSentencesAnnotator annotator = new WordsToSentencesAnnotator();
+		annotator.annotate(annotation);
+		
+		CoreDocument doc = new CoreDocument(annotation);
+		List<CoreSentence> phrases = doc.sentences();
+		int j = 0;
+		for(CoreSentence sentence : phrases)
+		{
+			j ++;
+			String sentenceText = sentence.text();
+			System.out.println("Phrase " + j + " : " + sentenceText);
+		}
+	}
+	
+	public static String lireExemple() throws IOException
+	{
+		String path = "corpus" + File.separator + "reference.txt";
+		FileInputStream is = new FileInputStream(path);     
+		String content = IOUtils.toString(is, "UTF-8");
+		return content;
+	}
 }
 
