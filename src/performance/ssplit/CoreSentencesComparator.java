6883a7e8119176177bfa94527a43f48d05211a3a
Poisson Blob <Poisson Blob@PoissonBlob-PC>
2018-05-02 18:20:54 +0200

    Documentation de CoreSentencesComparator

--- src/performance/ssplit/CoreSentencesComparator.java
+++ src/performance/ssplit/CoreSentencesComparator.java
@@ -3,21 +3,18 @@ package performance.ssplit;
 import java.util.List;
 import java.util.Properties;
 
-import org.apache.commons.io.IOUtils;
-
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.pipeline.*;
-import edu.stanford.nlp.util.CoreMap;
 import performance.Consts;
 import performance.stats.BasicStats;
 
-// Cette classe permet de comparer les r�sultats obtenus par l'annotateur ssplit de StanfordNLP
-// avec une r�f�rence qui correspond aux m�me textes annot�s manuellement
-public class CoreSentencesComparator //implements AnnotationComparator<CoreSentence>
+//@author Axel Clerici
+
+// Cette classe permet de comparer des phrases issues de Stanford NLP
+// avec des phrases de références déterminées manuellement
+public class CoreSentencesComparator
 {
 	private BasicStats stats;
 	private Properties props;
@@ -34,13 +31,16 @@ public class CoreSentencesComparator //implements AnnotationComparator<CoreSente
 		this.props = props;
 	}
 	
+	// Fonction générale qui va comparer les phrases de référence à celles de Stanford
+	// pour tous les textes du corpus. Elle met à jour des statistiques de base
+	// (vrai positif, faux positif, faux négatif, précision, rappel et f-mesure)
 	public BasicStats compareFiles() throws IOException
 	{			
 		File[] corpusFolder = new File(Consts.CORPUS_PATH).listFiles();
 
-		// Je n'ai pas encore annoté la main les autres fichiers
 		for(int i = 0; i < corpusFolder.length; i++)
 		{
+			// Pour chaque fichier, on récupère les phrases de références, celles de stanford, et on les compare
 			List<CoreSentence> stanfordSentences = SsplitUtils.getStanfordSentences(corpusFolder[i], props);
 			List<CoreSentence> referenceSentences = SsplitUtils.getCustomSentences(corpusFolder[i]);
 			compareFile(stanfordSentences, referenceSentences);
@@ -48,6 +48,8 @@ public class CoreSentencesComparator //implements AnnotationComparator<CoreSente
 		return this.stats;
 	}
 	
+	// Comparaison des phrases de référence et de stanford pour un texte du corpus.
+	// On considère ici la positision des tokens à laquelle s'effectue la coupure qui sépare deux phrases.
 	public void compareFile(List<CoreSentence> stanfordSentences, List<CoreSentence> referenceSentences) throws IOException
 	{
 		int tp = 0;
@@ -60,6 +62,8 @@ public class CoreSentencesComparator //implements AnnotationComparator<CoreSente
 			{
 				int sEnd = SsplitUtils.getEndCharOffsets(stanfordSentence);
 				int rEnd =  SsplitUtils.getEndCharOffsets(referenceSentence);
+				// Une coupure est présente à la même position dans Stanford et dans la référence :
+				// on compte un vrai positif
 				if(sEnd == rEnd)
 				{
 					tp ++;
@@ -67,9 +71,10 @@ public class CoreSentencesComparator //implements AnnotationComparator<CoreSente
 				}
 			}
 		}
+		// On retrouve faux positif et faux négatif à partir du nombre final de vrai positif
 		fp = stanfordSentences.size() - tp;
 		fn = referenceSentences.size() - tp;
-		
+		// on met à jour le compte
 		stats.updateStats(tp, fp, fn);
 	}
 }
